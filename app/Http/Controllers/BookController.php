860736a9d5cb849a2747ff57287116e779d4da04
10mi8o <totoma855@gmail.com>
2020-10-04 18:36:07 +0900

    一覧を取得するBookControllerメソッドを修正

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function getBooks() {
-      $books = Bool::all();
+      $books = Book::all();
       return response()->json($books, 200);
     }
 }
